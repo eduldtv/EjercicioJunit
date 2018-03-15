6b2beb23061d40e90227d55c37ac6f2800e32456
Edu <edu.ldtv@gmail.com>
2018-03-15 16:33:29 +0100

    Primer commit y push

---
+++ src/main/java/ejercicio/PersonaFiscal.java
@@ -0,0 +1,13 @@
+package ejercicio;
+
+public class PersonaFiscal {
+	private String nombre;
+	private int edad;
+	private int sueldo;
+
+	public int calcularImpuesto() {
+		return 1;
+	}
+
+	public PersonaFiscal()
+}
---
+++ src/main/java/ejercicio/TipoImpuesto.java
@@ -0,0 +1,8 @@
+package ejercicio;
+
+public class TipoImpuesto {
+	private int porcentaje;
+	private int porcentaje30;
+	private String nombre;
+
+}
